fc8c2d7890f0976823c454c890b2f52c9d848646
TARA BAHADUR PUN <59403192+puntara@users.noreply.github.com>
2020-02-10 17:41:44 -0800

    Add files via upload

---
+++ deleteDuplicate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def deleteDuplicate(M):
+    Array=[]
+    for i in N:
+        if(i not in Array):
+            Array.append(i)
+    return Array
+
+N=[1,1,2,2,3,3]
+z=deleteDuplicate(N)
+
+for i in z:
+    print(i)
+
